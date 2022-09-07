3d60d5275980a4e0e436686a6486beb50ff5ccde
Murad <67361462+Murad04@users.noreply.github.com>
2022-09-07 10:25:52 +0400

    Added comment

--- CleanArchitectureAPi.Api/Controllers/AuthenticationController.cs
+++ CleanArchitectureAPi.Api/Controllers/AuthenticationController.cs
@@ -16,11 +16,20 @@ public class AuthenticationController :ControllerBase
         _authenticationServices = authenticationServices;
     }
 
+    /**
+    * Register API Endpoint
+    * @param request The parameter for this Endpoint
+    */
     [HttpPost("register")]
     public IActionResult Register(RegisterRequest request)
     {
+        /**
+        *? Getting the result from IAuthenticationServices in order to check registering is completed.
+        */
         var result=_authenticationServices.Register(request.FirstName,request.LastName,request.Password,request.Email);
-
+        /** 
+        *?  Check the inputted data
+        */
         var response=new AuthenticationResponse(result.Id,result.FirstName,result.LastName,result.Email,result.Token);
 
         return Ok(response);
