e5c99a177593dca848abb0f7595e9e79ac97259d
Murad <67361462+Murad04@users.noreply.github.com>
2022-09-08 13:31:33 +0400

    Edited Errors

--- CleanArchitectureAPi.Domain/Common/Errors/Errors.Authentication.cs
+++ CleanArchitectureAPi.Domain/Common/Errors/Errors.Authentication.cs
@@ -6,6 +6,6 @@ public static partial class Errors
 {
     public static class Authentication
     {
-        public static Error InvalidCredentials=>Error.Validation(code:"Auth.Invalidcredentials",description:"Invalid credentials.");
+        public static Error InvalidCredentials=>Error.Validation(code:"Auth.Invalidcredentials",description:"Invalid credentials enterd.");
     }
 }
--- CleanArchitectureAPi.Domain/Common/Errors/Errors.User.cs
+++ CleanArchitectureAPi.Domain/Common/Errors/Errors.User.cs
@@ -6,6 +6,6 @@ public static partial class Errors
 {
     public static class User
     {
-        public static Error DuplicateEmail=>Error.Conflict(code:"User.DuplicateEmail",description:"Email already exists");
+        public static Error DuplicateEmail=>Error.Conflict(code:"User.DuplicateEmail",description:"Email already exists!");
     }
 }
